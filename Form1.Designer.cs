5e52cd4cc3c88429a6d6fa0d91ac7c9963db6e13
oykukapcak <oykukapcak@sabanciuniv.edu>
2016-05-24 12:46:45 +0200

    rock-paper-scissors client
    This is the client side of the game rock-paper-scissors.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace RPS_Step1_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "localhost";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 40);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            8888,
+            0,
+            0,
+            0});
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(259, 118);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 218);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ip of server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "port no";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Disconnect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "berke";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 253);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(207, 27);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Surrender";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 288);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿/*
+CS408 Term Project - Step 1
+Creators :  Öykü Kapcak
+            Berke Ataç
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RPS_Step1_Client
+{
+    public partial class Form1 : Form
+    {
+        bool terminating = false; 
+        Socket client;
+        Thread thrReceive;
+
+        string serverIP;
+        int serverPort;
+        string clientName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            button4.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        //on button click connects to the server with given ip and port no
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //takes inputs from textboxes and updown value box
+            serverIP = textBox1.Text;
+            serverPort = (int)numericUpDown1.Value;
+            clientName = textBox2.Text;
+
+            //connects user to server using sockets
+            client.Connect(serverIP, serverPort);
+
+            //starts a thread with receive function
+            thrReceive = new Thread(new ThreadStart(Receive));
+            thrReceive.IsBackground = true;
+            thrReceive.Start();
+
+            //Sends the name of the client to server
+            SendName(clientName);
+
+            //disables the connect button
+            button1.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        void SendMessage(string message)
+        {
+            //creates an array of bytes from the string input and proceeds to send it
+            byte[] buffer = Encoding.Default.GetBytes(message);
+            client.Send(buffer);
+            richTextBox1.Text+=("Your message has been sent.\n");
+        }
+
+        //this function sends the name of the client once it is connected.
+        void SendName(string message)
+        {
+            //creates an array of bytes from the string input and proceeds to send it
+            byte[] buffer = Encoding.Default.GetBytes(message);
+            client.Send(buffer);
+        }
+
+        //function is used in a thread and constantly checks for incoming buffers
+        private void Receive()
+        {
+            bool connected = true;
+            richTextBox1.Text+=("Connected to the server.\n");
+            
+            while (connected)
+            {
+                //while the connection has not ended
+                try
+                {
+                    //checks for incoming buffers
+                    byte[] buffer = new byte[256];
+
+                    int rec = client.Receive(buffer);
+
+                    if (rec <= 0)
+                    {
+                        throw new SocketException();
+                    }
+
+                    //decodes them to strings and gives output with the incoming string
+                    string newmessage = Encoding.Default.GetString(buffer);
+                    newmessage = newmessage.Substring(0, newmessage.IndexOf("\0"));
+                    richTextBox1.Text+=("Server: " + newmessage + "\r\n");
+                    if(newmessage == "Game has started!\n")
+                    {
+                        button4.Enabled = true;
+                    }
+                    if (newmessage.Length > 10 && newmessage.Substring(0,10) == "Game Over.")
+                    {
+                        button4.Enabled = false;
+                    }
+
+                }
+                catch
+                {
+                    if (!terminating)
+                        richTextBox1.Text+=("Connection has been terminated...\n");
+                    connected = false;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string message = textBox3.Text; //takes the string from textbox
+    
+            try
+            {
+                //on button click calls the message sending function with the input from text box
+                SendMessage(message);
+            }
+            catch
+            {
+                richTextBox1.Text += ("Your message could not be sent.\n");
+                richTextBox1.Text += ("connection lost...");
+                terminating = true;
+                client.Close();
+            }
+
+        }
+        
+
+        //on button click disconnects the user closing the socket
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += ("You have disconnected.\n");
+            terminating = true;
+            client.Close();
+
+            button3.Enabled = false;
+            button1.Enabled = true;
+
+            thrReceive.Abort();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SendMessage("SURRENDER");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            terminating = true;
+            client.Close();
+        }
+    }
+}
